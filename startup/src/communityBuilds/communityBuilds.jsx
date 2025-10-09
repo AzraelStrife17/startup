f210a0ea77de0d6257c706041ce3a949c4637340
AzraelStrife17 <ahri177511@gmail.com>
2025-10-08 20:11:33 -0600

    completed porting communitybuilds

--- startup/src/communityBuilds/communityBuilds.jsx
+++ startup/src/communityBuilds/communityBuilds.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './communityBuilds.css';
 
 export function CommunityBuilds() {
   return (
-    <main>
-      <div>community builds displayed here</div>
+   <main>
+        <span class="login_name">User Logged In</span>
+        <h1>Community Builds</h1>
+        <table class="community_builds_table">
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Build Name</th>
+              <th>Build ID</th>
+            </tr>
+          </thead>
+          <tbody>   
+            
+              <tr>
+                <td>
+                  <Link to='/characterBuilder'>Zer0</Link>
+                </td>
+                <td>
+                  <Link to='/characterBuilder'>Elite Endgame Build</Link>
+                </td>
+                <td>
+                  <Link to='/characterBuilder'>0123456789</Link>
+                </td>
+              </tr>
+            
+
+            
+              <tr>
+                <td>
+                  <Link to='/characterBuilder'>Vex</Link>
+                  <button type="submit">Vex</button>
+                </td>
+                <td>
+                  <Link to='/characterBuilder'>Best Deadringer Build</Link>
+                </td>
+                <td>
+                  <Link to='/characterBuilder'>0123456789</Link>
+                </td>
+              </tr>
+          </tbody>
+        </table>
+
+        <div class="search">
+          <input class="search_input" type="text" placeholder="search for builds by username, buildname, or ID"/>
+        </div>
     </main>
   );
 }
--- startup/src/savedBuilds/savedBuilds.jsx
+++ startup/src/savedBuilds/savedBuilds.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './savedBuilds.css';
 
 export function SavedBuilds() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>savedBuilds displayed here</div>
+    <main>
+        <span className="login_name">User Logged In</span>
+        <h1>My Builds</h1>
+        <table className="saved_builds_table">
+          <thead>
+            <th>Build Name</th>
+            <th>Build ID</th>
+          </thead>
+          <tbody>   
+            <form method = "get" action="characterBuilder.html">
+              <tr>
+                <td>
+                  <button type="submit">My Epic Build 1</button>
+                </td>
+                <td>
+                  <button type="submit">0123456789</button>
+                </td>
+              </tr>
+            </form>
+          </tbody>
+        </table>
+
+        <div className="search">
+          <input className="search_input" type="text" placeholder="search for your builds"/>
+        </div>
     </main>
   );
 }
